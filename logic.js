884ca074f931f48c1f1cd0871fb98fc93958a52c
Ali <ali@Alis-MacBook-Pro.local>
2018-03-19 12:54:34 -0400

    cleaner code

--- logic.js
+++ logic.js
@@ -1,24 +1,23 @@
-
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBpM4xMpjt3WeyDfnWMytMH16JSWmI1lt8",
     authDomain: "employee-list-5320b.firebaseapp.com",
     databaseURL: "https://employee-list-5320b.firebaseio.com",
     projectId: "employee-list-5320b",
     storageBucket: "employee-list-5320b.appspot.com",
     messagingSenderId: "388600441473"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
 
-$("#submit").on("click", function() {
+$("#submit").on("click", function () {
     event.preventDefault();
     console.log("this is a test");
 
     var name = $("#employee-name").val().trim();
-    var role =  $("#role").val().trim();
+    var role = $("#role").val().trim();
     var start = $("#start-date").val().trim();
     var rate = $("#monthly-rate").val().trim();
     database.ref().push({
@@ -32,7 +31,7 @@ $("#submit").on("click", function() {
 
 });
 
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", function (childSnapshot) {
     var newborn = childSnapshot.val()
     console.log(newborn.name);
     var row = $("<tr>");
@@ -43,24 +42,19 @@ database.ref().on("child_added", function(childSnapshot){
     var startDisplay = $("<td>");
     startDisplay.text(moment(newborn.start).format("MM/DD/YYYY"));
     var workDisplay = $("<td>");
-   // workDisplay.text(findMonthsWorked(newborn));
-   var monthsWorked = moment().diff(newborn.start, "months")
-   workDisplay.text(monthsWorked);
+    // workDisplay.text(findMonthsWorked(newborn));
+    var monthsWorked = moment().diff(newborn.start, "months")
+    workDisplay.text(monthsWorked);
 
     var rateDisplay = $("<td>");
     rateDisplay.text(newborn.rate);
     var billDisplay = $("<td>");
-    billDisplay.text(monthsWorked*newborn.rate);
+    billDisplay.text(monthsWorked * newborn.rate);
 
     row.append(nameDisplay, roleDisplay, startDisplay, workDisplay, rateDisplay, billDisplay);
     $("tbody").append(row);
 
 });
-
-var randomDate = "02/23/1999";
-var randomFormat = "MM/DD/YYYY";
-var convertedDate = moment(randomDate, randomFormat);
-
 
 /*
 
